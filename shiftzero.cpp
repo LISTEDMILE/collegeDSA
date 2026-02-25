2eee74a4a28945f1345bd426f0f0b53718cebc89
listedmile <listedmile@gmail.com>
2026-02-25 12:30:23 +0530

    go

---
+++ shiftzero.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int j = 0;  // position for next non-zero
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+}
+
+int main() {
+    vector<int> nums = {0,1,0,3,12};
+
+    moveZeroes(nums);
+
+    for (int x : nums)
+        cout << x << " ";
+}
